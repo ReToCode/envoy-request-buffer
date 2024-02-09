93701dc77703149e56ca1f457d6343c93455fe7e
Reto Lehmann <retocode@icloud.com>
2024-02-09 11:05:22 +0100

    add upstream test server

--- upstream/main.go
+++ upstream/main.go
@@ -4,11 +4,14 @@ import (
 	"flag"
 	"log"
 	"net"
+	"net/http"
 	"strconv"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
 )
 
 var (
@@ -17,13 +20,28 @@ var (
 )
 
 func main() {
+	go httpServer()
 	go grpcServer()
 	select {}
+}
+
+func httpServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from HTTP Server"))
+	})
+	srv := &http.Server{
+		Addr:         ":" + strconv.Itoa(*httpPort),
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+	log.Printf("Starting HTTP server on port: %d", *httpPort)
+	log.Fatal(srv.ListenAndServe())
 }
 
 func grpcServer() {
 	g := grpc.NewServer()
 	grpc_health_v1.RegisterHealthServer(g, health.NewServer())
+	reflection.Register(g)
 
 	lis, err := net.Listen("tcp", ":"+strconv.Itoa(*grpcPort))
 	if err != nil {
