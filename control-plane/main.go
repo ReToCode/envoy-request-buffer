0c2c2d7ed9029ae7ddeb21fbb65f25bc976194fa
Reto Lehmann <retocode@icloud.com>
2024-02-12 13:49:16 +0100

    Add dummy control-plane

--- control-plane/main.go
+++ control-plane/main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 )
 
+var scaledToZeroClusters = make(map[string]struct{})
+
+const httpPort = 7001
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from HTTP Server"))
-		w.WriteHeader(http.StatusOK)
-	})
+	http.HandleFunc("/scaled-to-zero-clusters", getScaledToZeroClusters)
+	http.HandleFunc("/set-scaled-to-zero", setScaledToZero)
 	srv := &http.Server{
-		Addr:         ":" + strconv.Itoa(*httpPort),
+		Addr:         ":" + strconv.Itoa(httpPort),
 		WriteTimeout: 5 * time.Second,
 		ReadTimeout:  5 * time.Second,
 	}
-	log.Printf("Starting HTTP server on port: %d", *httpPort)
+	log.Printf("Starting HTTP server on port: %d\n", httpPort)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func setScaledToZero(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("failed to POST parse form: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	hostname := r.Form.Get("host")
+
+	if hostname == "" {
+		log.Println("host not specified")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if _, has := scaledToZeroClusters[hostname]; has {
+		log.Printf("Removing %s from scaledToZeroClusters\n", hostname)
+		delete(scaledToZeroClusters, hostname)
+	} else {
+		log.Printf("Adding %s from scaledToZeroClusters\n", hostname)
+		scaledToZeroClusters[hostname] = struct{}{}
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func getScaledToZeroClusters(w http.ResponseWriter, r *http.Request) {
+	keys := make([]string, 0, len(scaledToZeroClusters))
+	for k := range scaledToZeroClusters {
+		keys = append(keys, k)
+	}
+	jsonStr, err := json.Marshal(keys)
+	if err != nil {
+		log.Println("failed to marshal scaledToZeroClusters, err: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	log.Printf("getScaledToZeroClusters called, returning: %s\n", jsonStr)
+	_, err = w.Write(jsonStr)
+	if err != nil {
+		log.Printf("failed to write to output stream, err: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
